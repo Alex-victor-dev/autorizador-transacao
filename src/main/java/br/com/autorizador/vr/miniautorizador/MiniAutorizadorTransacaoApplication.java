2f533cbc4b00fb4e71bff3cebd14acc32dda6d66
Alex-Victor <alexvictoribeiro@gmail.com>
2022-11-23 08:30:15 -0300

    init commit

---
+++ src/main/java/br/com/autorizador/vr/miniautorizador/MiniAutorizadorTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.autorizador.vr.miniautorizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/autorizador/vr/miniautorizador/MiniAutorizadorTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autorizador.vr.miniautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
