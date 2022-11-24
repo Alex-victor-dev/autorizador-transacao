7deea9f3801e27bb3142e6b63bce5e77e48859bb
Alex-Victor <alexvictoribeiro@gmail.com>
2022-11-24 09:28:47 -0300

    faz tratamento de erros

--- src/main/java/br/com/autorizador/vr/miniautorizador/cartao/application/service/CartaoApplicationService.java
+++ src/main/java/br/com/autorizador/vr/miniautorizador/cartao/application/service/CartaoApplicationService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ import br.com.autorizador.vr.miniautorizador.cartao.domain.Cartao;
 import br.com.autorizador.vr.miniautorizador.cartao.domain.StatusCompra;
 import br.com.autorizador.vr.miniautorizador.cartao.domain.Transacao;
 import br.com.autorizador.vr.miniautorizador.cliente.application.service.ClienteService;
+import br.com.autorizador.vr.miniautorizador.cliente.handler.APIException;
 import br.com.autorizador.vr.miniautorizador.cliente.handler.HandleException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -43,6 +45,9 @@ public class CartaoApplicationService implements CartaoService {
 	public CartaoDetalhadoResponse getCartaoPorNumero(String numeroCartao) {
 		log.info("[inicia] CartaoApplicationService - getCartaoPorNumero");
 		Cartao cartao = cartaoRepository.buscaCartaoPorNumero(numeroCartao);
+		if (cartao == null) {
+			throw APIException.build(HttpStatus.NOT_FOUND, "Cartao Nao Encontrado, Verifique o Numero do Cartao");
+		}
 		log.info("[finaliza] CartaoApplicationService - getCartaoPorNumero");
 		return new CartaoDetalhadoResponse(cartao);
 	}
@@ -53,7 +58,15 @@ public class CartaoApplicationService implements CartaoService {
 			String senha) {
 		log.info("[inicia] CartaoApplicationService - realizaTransacao");
 		Cartao cartao = cartaoRepository.buscaCartaoPorNumero(numeroCartao);
+		if (cartao == null) {
+			throw APIException.build(HttpStatus.UNAUTHORIZED,
+					"Imposivel Realizar a Transacao Numero do cartao Inexistente");
+
+		}
 		Transacao transacao = cartaoRepository.salvaTransacao(new Transacao(transacaoRequest));
+		if (cartao.getLimiteCartao() < transacao.getValorCompra()) {
+			throw APIException.build(HttpStatus.UNAUTHORIZED, "Voce nao tem limite Para realizar a transacao");
+		}
 		cartao.validarAtribuirNovoSaldo(transacao.getValorCompra(), handleException);
 		cartaoRepository.salvaCartao(cartao);
 		log.info("[finaliza] CartaoApplicationService - realizaTransacao");
